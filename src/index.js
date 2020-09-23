6584024b14abdc412a22ff3ca9f5b504dd815d16
Itumeleng <thiotsoela@gmail.com>
2020-09-23 11:30:29 +0200

    Move Components into Separate Files

--- src/index.js
+++ src/index.js
@@ -1,22 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-const theRoot = document.getElementById("root");
+import MyInfo from "./components/MyInfo";
 
-ReactDOM.render(<MyInfo />, theRoot);
-
-function MyInfo() {
-  return (
-    <div>
-      <h1>Hi, my name is Itumeleng</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero soluta
-        officia praesentium quas eum hic.
-      </p>
-      <ul>
-        <li>Something 1</li>
-        <li>Something 2</li>
-        <li>Something 3</li>
-      </ul>
-    </div>
-  );
-}
+ReactDOM.render(<MyInfo />, document.getElementById("root"));
