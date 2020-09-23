6d5a843af95dbc0dbb1aebe5a2783d595a6c9399
Itumeleng <thiotsoela@gmail.com>
2020-09-23 12:43:55 +0200

    Parent/Child Components Part 1

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div>
+      <h1>I am a Footer</h1>
+      <a>Home</a>
+      <a>About</a>
+      <a>Contact</a>
+    </div>
+  );
+}
