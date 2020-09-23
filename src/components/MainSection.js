588a66605e40cac1053748a729236a6eb7092537
Itumeleng <thiotsoela@gmail.com>
2020-09-23 15:09:12 +0200

    JSX to JavaScript

--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -1,11 +1,14 @@
 import React from "react";
 
 export default function MainSection() {
+  const firstName = "Thio";
+  const lastName = "Mark";
+  // Adding javascript we use {}*/
+
   return (
     <main>
-      <input type="checkbox" />
+      <h1>Hello my name is {firstName + " " + lastName}</h1>
       <p>
-        {" "}
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis
         temporibus aliquam fugit placeat, sit iste assumenda illo quo
         perspiciatis nemo.
