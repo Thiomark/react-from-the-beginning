d58c78ce0048d637ead24d2e40c9a2f73235ada4
Itumeleng <thiotsoela@gmail.com>
2020-09-23 15:19:14 +0200

    inline style

--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -3,10 +3,17 @@ import React from "react";
 export default function MainSection() {
   const firstName = "Thio";
   const lastName = "Mark";
-  // Adding javascript we use {}*/
+  // Adding javascript we use {}
+
+  // We use an object to add inline style to anything
+  const inlineStyle = {
+    backgroundColor: "gray",
+    color: "orangered",
+    fontSize: "14px",
+  };
 
   return (
-    <main>
+    <main style={inlineStyle}>
       <h1>Hello my name is {firstName + " " + lastName}</h1>
       <p>
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis
