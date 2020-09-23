ced220dfcb269a59b446706e9de195f7db256236
Itumeleng <thiotsoela@gmail.com>
2020-09-23 11:22:22 +0200

    Functional Components

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
+const theRoot = document.getElementById("root");
 
-ReactDOM.render(<h1>Hello world</h1>, document.getElementById("root"));
+ReactDOM.render(<MyInfo />, theRoot);
+
+function MyInfo() {
+  return (
+    <div>
+      <h1>Hi, my name is Itumeleng</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero soluta
+        officia praesentium quas eum hic.
+      </p>
+      <ul>
+        <li>Something 1</li>
+        <li>Something 2</li>
+        <li>Something 3</li>
+      </ul>
+    </div>
+  );
+}
