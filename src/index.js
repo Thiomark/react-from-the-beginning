3c638ecc1a362b0ecc529541a60b603918a623c0
Itumeleng <thiotsoela@gmail.com>
2020-09-23 11:20:36 +0200

    Hello world

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(what gets rendered, where it will get rendered);
-
-
+ReactDOM.render(<h1>Hello world</h1>, document.getElementById("root"));
